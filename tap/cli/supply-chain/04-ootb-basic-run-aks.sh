a0866f7b0edaed74d6ac45be0a351a09502d027a
Michael James <mjames@pivotal.io>
2023-07-04 12:44:13 -0400

    Added AKS cluster to view script.

--- tap/cli/supply-chain/04-ootb-basic-run-aks.sh
+++ tap/cli/supply-chain/04-ootb-basic-run-aks.sh
@@ -3,7 +3,7 @@
 
 TAP_VERSION=1.5.0
 VIEW_DOMAIN=view.tap.nycpivot.com
-RUN_DOMAIN=run.tap.nycpivot.com
+RUN_DOMAIN=run-aks.tap.nycpivot.com
 
 tap_run=tap-run-aks
 
@@ -20,6 +20,8 @@ rm tap-values-run.yaml
 cat <<EOF | tee tap-values-run.yaml
 profile: run
 ceip_policy_disclosed: true
+shared:
+  ingress_domain: $RUN_DOMAIN
 supply_chain: basic
 contour:
   infrastructure_provider: aws
@@ -37,3 +39,49 @@ excluded_packages:
 EOF
 
 tanzu package install tap -p tap.tanzu.vmware.com -v $TAP_VERSION --values-file tap-values-run.yaml -n tap-install
+
+#CONFIGURE DNS NAME WITH ELB IP
+echo
+echo "Press Ctrl+C when contour packages has successfully reconciled"
+echo
+
+kubectl get pkgi -n tap-install -w | grep contour
+
+echo
+echo "<<< CONFIGURING DNS >>>"
+echo
+
+sleep 5
+
+hosted_zone_id=$(aws route53 list-hosted-zones --query HostedZones[0].Id --output text | awk -F '/' '{print $3}')
+ingress=$(kubectl get svc envoy -n tanzu-system-ingress -o json | jq -r .status.loadBalancer.ingress[].hostname)
+
+echo $ingress
+echo
+
+#rm change-batch.json
+change_batch_filename=change-batch-$RANDOM
+cat <<EOF | tee $change_batch_filename.json
+{
+    "Comment": "Update record.",
+    "Changes": [
+        {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "*.$RUN_DOMAIN",
+                "Type": "CNAME",
+                "TTL": 60,
+                "ResourceRecords": [
+                    {
+                        "Value": "$ingress"
+                    }
+                ]
+            }
+        }
+    ]
+}
+EOF
+echo
+
+echo $change_batch_filename.json
+aws route53 change-resource-record-sets --hosted-zone-id $hosted_zone_id --change-batch file:///$HOME/$change_batch_filename.json
