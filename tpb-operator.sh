048bc3019b2075a793d84154dba1fd7e299d0192
Michael James <mijames@vmware.com>
2023-08-12 19:28:31 -0400

    Added more to tdp.

--- tpb-operator.sh
+++ tpb-operator.sh
@@ -1,1 +1,1 @@
-ssh ubuntu@ec2-34-224-86-211.compute-1.amazonaws.com -i operator/keys/tpb-operator-keypair.pem -L 3000:localhost:3000
+ssh ubuntu@ec2-52-23-231-53.compute-1.amazonaws.com -i operator/keys/tpb-operator-keypair.pem -L 3000:localhost:3000
