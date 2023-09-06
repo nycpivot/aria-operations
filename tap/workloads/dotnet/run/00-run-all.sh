d7b48614108a49cd277c93d1716895e5409ab23e
Michael James <mijames@vmware.com>
2023-09-05 20:42:57 -0400

    Refactored build scripts.

--- tap/workloads/dotnet/run/00-run-all.sh
+++ tap/workloads/dotnet/run/00-run-all.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-tap_build=tap-build
-
 tap_dotnet_weather_web=tap-dotnet-weather-web
 tap_dotnet_weather_api=tap-dotnet-weather-api
 tap_dotnet_weather_data=tap-dotnet-weather-data
 
-
-# tap-build
-kubectl config use-context ${tap_build}
-
-kubectl delete all -l operations=aria
+bash 00-reset-all.sh
+bash 01-run-tap-dotnet-weather-web.sh
+bash 02-run-tap-dotnet-weather-api.sh
+bash 03-run-tap-dotnet-weather-data.sh
---
+++ tap/workloads/dotnet/run/01-run-tap-dotnet-weather-web.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+tap_build=tap-build
+tap_run_eks=tap-run-eks
+tap_dotnet_weather_web=tap-dotnet-weather-web
+
+kubectl config use-context ${tap_build}
+
+if [ ! -d ${HOME}/${tap_dotnet_weather_web} ]
+then
+  mkdir ${HOME}/${tap_dotnet_weather_web}
+fi
+
+if test -f "${HOME}/${tap_dotnet_weather_web}/${tap_dotnet_weather_web}-deliverable.yaml"; then
+  rm ${HOME}/${tap_dotnet_weather_web}/${tap_dotnet_weather_web}-deliverable.yaml
+  echo
+fi
+
+kubectl get configmap ${tap_dotnet_weather_web}-deliverable -o go-template='{{.data.deliverable}}' \
+    > ${HOME}/${tap_dotnet_weather_web}/${tap_dotnet_weather_web}-deliverable.yaml
+
+kubectl config use-context ${tap_run_eks}
+
+# wavefront secret
+wavefront_api_secret=wavefront-api-secret
+
+wavefront_url=$(aws secretsmanager get-secret-value --secret-id aria-operations | jq -r .SecretString | jq -r .\"wavefront-prod-url\")
+wavefront_token=$(aws secretsmanager get-secret-value --secret-id aria-operations | jq -r .SecretString | jq -r .\"wavefront-prod-token\")
+
+kubectl delete -f ${HOME}/${tap_dotnet_weather_web}/${wavefront_api_secret}.yaml
+if test -f "${HOME}/${tap_dotnet_weather_web}/${wavefront_api_secret}.yaml"; then
+  rm ${HOME}/${tap_dotnet_weather_web}/${wavefront_api_secret}.yaml
+fi
+
+cat <<EOF | tee ${HOME}/${tap_dotnet_weather_web}/${wavefront_api_secret}.yaml
+apiVersion: v1
+kind: Secret
+metadata:
+  name: ${wavefront_api_secret}
+  labels:
+    operations: aria
+type: Opaque
+stringData:
+  host: ${wavefront_url}
+  token: ${wavefront_token}
+EOF
+
+kubectl apply -f ${HOME}/${tap_dotnet_weather_web}/${wavefront_api_secret}.yaml
+
+# give services toolkit permission to view secrets
+stk_cluster_role=stk-cluster-role
+
+kubectl delete -f ${HOME}/${tap_dotnet_weather_web}/${stk_cluster_role}.yaml --ignore-not-found
+if test -f "${HOME}/${tap_dotnet_weather_web}/${stk_cluster_role}.yaml"; then
+  rm ${HOME}/${tap_dotnet_weather_web}/${stk_cluster_role}.yaml
+fi
+
+cat <<EOF | tee ${HOME}/${tap_dotnet_weather_web}/${stk_cluster_role}.yaml
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  name: ${stk_cluster_role}
+  labels:
+    servicebinding.io/controller: "true"
+    operations: aria
+rules:
+- apiGroups:
+  - ""
+  resources:
+  - secrets
+  verbs:
+  - get
+  - list
+  - watch
+EOF
+
+kubectl apply -f ${HOME}/${tap_dotnet_weather_web}/${stk_cluster_role}.yaml
+
+redis_cache_class_claim=redis-cache-class-claim
+wavefront_api_resource_claim=wavefront-api-resource-claim
+
+kubectl delete classclaim ${redis_cache_class_claim} --ignore-not-found
+kubectl delete resourceclaim ${wavefront_api_resource_claim} --ignore-not-found
+
+tanzu service class-claim create ${redis_cache_class_claim} \
+  --class redis-unmanaged --parameter storageGB=1
+tanzu service resource-claim create ${wavefront_api_resource_claim} \
+  --resource-name ${wavefront_api_secret} --resource-kind Secret --resource-api-version v1
+
+kubectl delete -f ${HOME}/${tap_dotnet_weather_web}/${tap_dotnet_weather_web}-deliverable.yaml --ignore-not-found
+kubectl apply -f ${HOME}/${tap_dotnet_weather_web}/${tap_dotnet_weather_web}-deliverable.yaml
+
+echo
+echo ">>> Running Workloads:"
+
+echo https://${tap_dotnet_weather_web}.default.${tap_run_eks_domain}.tap.nycpivot.com
+echo
